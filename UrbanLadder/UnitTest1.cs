3ec91eb34c1e05e0ebcc704fe6ac5010b7a27ece
187525 <187525@ust-global.com>
2023-11-30 11:58:47 +0530

    Add project files.

---
+++ UrbanLadder/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UrbanLadder
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
